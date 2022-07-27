6987e0d2d0cab914cf253b93e30c07b5af3c80fb
Alphen <tempalora@hotmail.com>
2022-07-27 10:54:19 +0300

    Version 0.2!
    Fixed CSS conflicts, like, a lot of 'em!
    
    Co-Authored-By: Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>

--- 1. Classic Edition/index.js
+++ 1. Classic Edition/index.js
@@ -1,67 +1,55 @@
-var width = $(window).width(); 
-window.onscroll = function(){
-if ((width >= 1000)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#header").css("background","#fff");
-        $("#header").css("color","#000");
-        $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
-        $("#header").css("padding","4vh 4vw");
-        $("#navigation a").hover(function(){
-            $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
-        },function(){
-            $(this).css("border-bottom","2px solid transparent");
-        });
-    }else{
-        $("#header").css("background","transparent");
-        $("#header").css("color","#fff");
-        $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
-        $("#header").css("padding","6vh 4vw");
-        $("#navigation a").hover(function(){
-            $(this).css("border-bottom","2px solid #fff");
-        },function(){
-            $(this).css("border-bottom","2px solid transparent");
-        });
+const loadingContainer = document.getElementById('loading');
+const header = document.getElementsByTagName('header')[0];
+
+window.addEventListener('scroll', () => {
+    if (window.innerWidth >= 1000) {
+        document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ? header.classList.add('isScrolled') : header.classList.remove('isScrolled')
     }
-}
-}
+})
 
-function magnify(imglink){
-    $("#img_here").css("background",`url('${imglink}') center center`);
-    $("#magnify").css("display","flex");
-    $("#magnify").addClass("animated fadeIn");
-    setTimeout(function(){
-        $("#magnify").removeClass("animated fadeIn");
-    },800);
-}
 
-function closemagnify(){
-    $("#magnify").addClass("animated fadeOut");
-    setTimeout(function(){
-        $("#magnify").css("display","none");
-        $("#magnify").removeClass("animated fadeOut");
-        $("#img_here").css("background",`url('') center center`);
-    },800);
-}
 
-setTimeout(function(){
+setTimeout(function () {
     $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-    },800);
-},1650);
+    setTimeout(function () {
+        $("#loading").removeClass("animated fadeOut");
+        $("#loading").css("display", "none");
+    }, 800);
+}, 300);
 
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('body,html').animate({
-        scrollTop: $(hash).offset().top
-        }, 1800, function(){
-        window.location.hash = hash;
-       });
-       } 
-      });
-  });
-  
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('body,html').animate({
+                scrollTop: $(hash).offset().top
+            }, 1800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+});
+
+const slides = document.querySelectorAll('.slide')
+
+slides.forEach((slide) => {
+    slide.addEventListener('click', () => {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+        slide.classList.add('active')
+    })
+})
+
+// The classic 'todo fix'
+const accordeonsOrHowererYouSpellThisShit = document.getElementsByClassName('card-header');
+
+for (let i; i < accordeonsOrHowererYouSpellThisShit.length; i++) {
+    console.log(i)
+    accordeonsOrHowererYouSpellThisShit[i].addEventListener('click' , (thing) => {
+        console.log('wwwwwwjfslkdjfklsajdflaksjf;lkasdj')
+        thing.target.nextSibling.toggle('active')
+    })
+}
